9cc1d53ea8d140e215b7120f1c192aeb3d8ab256
Andreas Suryadi <andreas.suryadi02@gmail.com>
2020-12-04 21:06:52 +0700

    UTS-IF10K-10119918-ANDREASSURYADI-no3

---
+++ src/uts/if10k/pkg10119918/andreassuryadi/no3/UTSIF10K10119918ANDREASSURYADINo3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if10k.pkg10119918.andreassuryadi.no3;
+
+/**
+ *
+ * @author 
+ * NAMA      : Andreas Suryadi
+ * KELAS     : IF-10K
+ * NIM       : 10119918
+ * Deskripsi Program : Program ini bertujuan untuk menampilkan data nama musisi
+ * dan genre
+ * 
+ */
+
+public class UTSIF10K10119918ANDREASSURYADINo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
